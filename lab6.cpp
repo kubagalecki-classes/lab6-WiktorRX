bf51fa1bfb767e5f901b3a5be4320c52e9195e9f
WiktorRX <wiktorm@live.com>
2020-12-15 21:11:59 +0000

    12152211

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,20 @@
+#include "make_random_vector.hpp"
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main()
 {
-    puts("Hello, World!");
+    int           T_size = 20;
+    vector< int > Blabla{make_random_vector(T_size, 0, 10)};
+
+    for (int i = 0; i < T_size; i++) {
+        cout << Blabla[i] << endl;
+    }
+    cout << "__________________________________" << endl;
+    sort(Blabla.begin(), Blabla.begin() + T_size);
+
+    for (int i = 0; i < T_size; i++) {
+        cout << Blabla[i] << endl;
+    }
+    cout << "ilość siódemek: " << count(Blabla.begin(), Blabla.begin() + T_size, 7) << endl;
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -14,3 +14,21 @@ std::vector< char > foo(std::list< Human >& people)
 
     return ret_v;
 }
+
+
+
+/*{
+    std::vector<char> ludzie(people.size());
+    std::list<Human>::iterator it = people.begin();
+    std::vector<char>::reverse_iterator rit=ludzie.rbegin();
+    for(it;it!=people.end();it++)
+    {
+      it->birthday();
+      if (it->isMonster()==true)
+      {*rit = 'n';}
+      else
+      {*rit='y';}
+      rit++;
+    }
+    return ludzie;
+}*/
