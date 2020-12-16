f43426033794c011a85833a1a50047a946ab465e
WiktorRX <wiktorm@live.com>
2020-12-16 22:56:29 +0000

    blabla

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -3,16 +3,15 @@
 #include "Human.hpp"
 
 #include <algorithm>
-//#include <iostream>
 #include <list>
 #include <vector>
-// using namespace std;
 
-int main()
-
-    std::vector< char > foo(std::list< Human >& people)
+std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
+
+    // Twoja implementacja tutaj
+
     std::for_each(people.begin(), people.end(), [](Human& people) { people.birthday(); });
     std::transform(people.begin(), people.end(), ret_v.begin(), [](Human& people) -> char {
         if (people.isMonster() == true)
@@ -20,23 +19,8 @@ int main()
         else
             return 'y';
     });
-    std::reverse(ret_v.begin(), ret_v.end());
 
+    std::reverse(ret_v.begin(), ret_v.end());
+    
     return ret_v;
 }
-
-/*{
-    std::vector<char> ludzie(people.size());
-    std::list<Human>::iterator it = people.begin();
-    std::vector<char>::reverse_iterator rit=ludzie.rbegin();
-    for(it;it!=people.end();it++)
-    {
-      it->birthday();
-      if (it->isMonster()==true)
-      {*rit = 'n';}
-      else
-      {*rit='y';}
-      rit++;
-    }
-    return ludzie;
-}*/
